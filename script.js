40608bbe8fb69c7b15318050f8ec6ac53e630a1e
firkaak <millok.tamas3@gmail.com>
2023-08-11 13:14:21 +0200

    added image to the devVerz1 branch

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const myList = document.querySelector('#myList');
 let title = document.querySelector('#title').innerHTML;
-console.log(title);
+// console.log(title);
 const titleArray = title.split(" ");
-console.log(titleArray);
+// console.log(titleArray);
 let li1 = document.createElement('li');
 let li2 = document.createElement('li');
 let li3 = document.createElement('li');
@@ -22,3 +22,10 @@ myList.append(li1);
 myList.append(li2);
 myList.append(li3);
 myList.append(li4);
+
+// adding image to the page
+
+let imageHolder = document.querySelector('#mainImg');
+let image = document.createElement('img');
+image.setAttribute('src', 'images/Paper.Demo.6.png');
+imageHolder.append(image);
