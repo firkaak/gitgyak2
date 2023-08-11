209f0d83a90440ebca6d4c480bb513b559225857
firkaak <millok.tamas3@gmail.com>
2023-08-11 12:07:40 +0200

    added a list with the title in the li elements, with help of javascript

---
+++ script.js
@@ -0,0 +1,24 @@
+const myList = document.querySelector('#myList');
+let title = document.querySelector('#title').innerHTML;
+console.log(title);
+const titleArray = title.split(" ");
+console.log(titleArray);
+let li1 = document.createElement('li');
+let li2 = document.createElement('li');
+let li3 = document.createElement('li');
+let li4 = document.createElement('li');
+
+let li1Text = titleArray[0];
+let li2Text = titleArray[1];
+let li3Text = titleArray[2];
+let li4Text = titleArray[3];
+
+li1.append(li1Text);
+li2.append(li2Text);
+li3.append(li3Text);
+li4.append(li4Text);
+
+myList.append(li1);
+myList.append(li2);
+myList.append(li3);
+myList.append(li4);
